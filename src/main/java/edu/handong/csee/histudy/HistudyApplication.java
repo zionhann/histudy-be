b20d1925f0d8775e1c33028a901c0043ec620ad4
zionhann <hanzion1108@gmail.com>
2023-05-17 12:50:36 +0900

    :tada: Initial setup

---
+++ src/main/java/edu/handong/csee/histudy/HistudyApplication.java
@@ -0,0 +1,13 @@
+package edu.handong.csee.histudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HistudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/handong/csee/histudy/HistudyApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.handong.csee.histudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
