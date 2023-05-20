a4ab4ceb09d16171f6428d012a52576d8b462496
zionhann <hanzion1108@gmail.com>
2023-05-20 14:44:28 +0900

    Add: 보고서 이미지 엔티티 생성 (#2)

---
+++ src/main/java/edu/handong/csee/histudy/domain/Image.java
@@ -0,0 +1,25 @@
+package edu.handong.csee.histudy.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String path;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Report report;
+
+    @Builder
+    public Image(String path, Report report) {
+        this.path = path;
+        this.report = report;
+    }
+}
