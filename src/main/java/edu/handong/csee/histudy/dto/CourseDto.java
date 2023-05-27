e6947f5b200544ea960928d08fdfccd6cd86d8d0
juyoung88 <76582732+juyoung88@users.noreply.github.com>
2023-05-28 04:11:15 +0900

    Add: Created CourseDto (#18)

---
+++ src/main/java/edu/handong/csee/histudy/dto/CourseDto.java
@@ -0,0 +1,18 @@
+package edu.handong.csee.histudy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CourseDto {
+    private String name;
+    private String professor;
+    private String code;
+    private int year;
+    private int semester;
+}
