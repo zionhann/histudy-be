10fc8095daa21831a7f7cc5b835b65c454f066e8
zionhann <hanzion1108@gmail.com>
2023-05-30 23:03:27 +0900

    Feat: 액세스 토큰 만료시 토큰 재발급 (#14)

--- src/main/java/edu/handong/csee/histudy/controller/AuthController.java
+++ src/main/java/edu/handong/csee/histudy/controller/AuthController.java
@@ -1,17 +1,18 @@
 package edu.handong.csee.histudy.controller;
 
+import edu.handong.csee.histudy.controller.form.TokenForm;
 import edu.handong.csee.histudy.domain.User;
 import edu.handong.csee.histudy.dto.UserDto;
+import edu.handong.csee.histudy.jwt.GrantType;
 import edu.handong.csee.histudy.jwt.JwtPair;
+import edu.handong.csee.histudy.jwt.TokenInfo;
 import edu.handong.csee.histudy.service.JwtService;
 import edu.handong.csee.histudy.service.UserService;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -43,5 +44,25 @@ public class AuthController {
                 .body(UserDto.Login.builder()
                         .isRegistered(false)
                         .build());
+    }
+
+    @PostMapping("/token")
+    public ResponseEntity<TokenInfo> issueAccessToken(@RequestBody TokenForm tokenForm) {
+        Optional<String> refreshTokenOr = Optional.ofNullable(tokenForm.getRefreshToken());
+        Optional<Claims> claimsOr = jwtService.validate(refreshTokenOr);
+
+        if (refreshTokenOr.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .build();
+        } else if (claimsOr.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .build();
+        }
+        Claims claims = claimsOr.get();
+        String email = claims.getSubject();
+        String name = claims.get("name", String.class);
+        String accessToken = jwtService.issueToken(email, name, GrantType.ACCESS_TOKEN);
+
+        return ResponseEntity.ok(new TokenInfo(GrantType.ACCESS_TOKEN, accessToken));
     }
 }
--- src/test/java/edu/handong/csee/histudy/auth/AuthControllerTests.java
+++ src/test/java/edu/handong/csee/histudy/auth/AuthControllerTests.java
@@ -1,20 +1,25 @@
 package edu.handong.csee.histudy.auth;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.handong.csee.histudy.controller.form.TokenForm;
 import edu.handong.csee.histudy.domain.User;
 import edu.handong.csee.histudy.dto.UserDto;
+import edu.handong.csee.histudy.jwt.GrantType;
+import edu.handong.csee.histudy.jwt.TokenInfo;
 import edu.handong.csee.histudy.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -69,5 +74,59 @@ public class AuthControllerTests {
         assertNotNull(res);
         assertEquals("Bearer ", res.getTokenType());
         assertTrue(res.getIsRegistered());
+    }
+
+    @DisplayName("토큰 재발급: 유효하지 않은 Refresh token")
+    @Test
+    void AuthControllerTests_76() throws Exception {
+        // given
+        String invalidJwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." +
+                "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ." +
+                "SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+        TokenForm tokenForm = new TokenForm(GrantType.REFRESH_TOKEN.name(), invalidJwt);
+        String form = mapper.writeValueAsString(tokenForm);
+
+        // when
+        mvc
+                .perform(post("/api/auth/token")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(form))
+                .andExpect(status().isUnauthorized())
+                .andDo(print());
+    }
+
+    @DisplayName("토큰 재발급: 페이로드가 없음")
+    @Test
+    void AuthControllerTests_86() throws Exception {
+        mvc
+                .perform(post("/api/auth/token"))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+
+    @DisplayName("토큰 재발급: 유효한 요청")
+    @Test
+    void AuthControllerTests_96() throws Exception {
+        // given
+        String jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." +
+                "eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAiLCJzdWIiOiJ0ZXN0QGV4YW1wbGUuY29tIiwibmFtZSI6InVzZXIifQ." +
+                "ZB9yuKUvuufMfbXkzB647PoSCw-3rc2FA-PoHzKLYgM";
+        TokenForm tokenForm = new TokenForm(GrantType.REFRESH_TOKEN.name(), jwt);
+        String form = mapper.writeValueAsString(tokenForm);
+
+        // when
+        MvcResult mvcResult = mvc
+                .perform(post("/api/auth/token")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(form))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+        TokenInfo res = mapper.readValue(mvcResult.getResponse().getContentAsString(),
+                TokenInfo.class);
+
+        // then
+        assertEquals(GrantType.ACCESS_TOKEN, res.getGrantType());
     }
 }
