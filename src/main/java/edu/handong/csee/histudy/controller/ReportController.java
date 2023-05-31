0ccd88e89ba144a8c6164d76b32a4e8949959d7f
juyoung88 <76582732+juyoung88@users.noreply.github.com>
2023-05-31 19:12:12 +0900

    Feat: Changed API (commented out existing api for possible errors) (#22)

--- src/main/java/edu/handong/csee/histudy/controller/ReportController.java
+++ src/main/java/edu/handong/csee/histudy/controller/ReportController.java
@@ -21,22 +21,22 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ReportController {
 
-    private final ReportRepository reportRepository;
+//    private final ReportRepository reportRepository;
     private final ReportService reportService;
-    private final UserRepository userRepository;
+//    private final UserRepository userRepository;
 
+//    @PostMapping("/api/v1/report")
+//    public ReportDto.Response createReport(@RequestBody ReportForm form,
+//                                           @RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken) {
+//        User user = userRepository.findUserByAccessToken(accessToken).orElseThrow();
+//        List<Optional<User>> participants = form.getParticipants().stream()
+//                .map(userRepository::findUserBySid)
+//                .toList();
+//        Report report = form.toEntity(user.getTeam(), participants);
+//
+//        return new ReportDto.Response(reportRepository.save(report));
+//    }
     @PostMapping("/api/v1/report")
-    public ReportDto.Response createReport(@RequestBody ReportForm form,
-                                           @RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken) {
-        User user = userRepository.findUserByAccessToken(accessToken).orElseThrow();
-        List<Optional<User>> participants = form.getParticipants().stream()
-                .map(userRepository::findUserBySid)
-                .toList();
-        Report report = form.toEntity(user.getTeam(), participants);
-
-        return new ReportDto.Response(reportRepository.save(report));
-    }
-    @PostMapping("/api/v1/reportCourse")
     public ReportDto.Response createReportCourse(@RequestBody ReportForm form,
                                                  @RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken) {
         return reportService.createReport(form,accessToken);
