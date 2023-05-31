d0b26c0290857ecb014787a54d52c5a05fa104d3
juyoung88 <76582732+juyoung88@users.noreply.github.com>
2023-05-31 20:56:46 +0900

    Add: Added ChoiceRepository (#23)

---
+++ src/main/java/edu/handong/csee/histudy/repository/ChoiceRepository.java
@@ -0,0 +1,7 @@
+package edu.handong.csee.histudy.repository;
+
+import edu.handong.csee.histudy.domain.Choice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChoiceRepository extends JpaRepository<Choice,Long> {
+}
