99cfc789a4d25b61a8073ea1c3a6c7a873ff91c7
juyoung88 <76582732+juyoung88@users.noreply.github.com>
2023-05-31 23:30:45 +0900

    Fix: Fix test conflict

--- src/test/java/edu/handong/csee/histudy/report/ReportServiceTest.java
+++ src/test/java/edu/handong/csee/histudy/report/ReportServiceTest.java
@@ -1,8 +1,10 @@
 package edu.handong.csee.histudy.report;
 
 import edu.handong.csee.histudy.controller.form.ReportForm;
+import edu.handong.csee.histudy.domain.Course;
 import edu.handong.csee.histudy.domain.Role;
 import edu.handong.csee.histudy.domain.Team;
+import edu.handong.csee.histudy.repository.CourseRepository;
 import edu.handong.csee.histudy.repository.TeamRepository;
 import edu.handong.csee.histudy.domain.User;
 import edu.handong.csee.histudy.dto.ReportDto;
@@ -24,12 +26,37 @@ public class ReportServiceTest {
     @Autowired
     UserRepository userRepository;
     @Autowired
-    private TeamRepository teamRepository;
+    TeamRepository teamRepository;
+    @Autowired
+    CourseRepository courseRepository;
 
     @DisplayName("보고서 생성시 과목을 선택할 수 있다")
     @Transactional
     @Test
     public void reportServiceTest() {
+        Course course = Course.builder()
+                .name("기초전자공학실험")
+                .code("ECE20007")
+                .courseYear(2023)
+                .semester(1)
+                .build();
+        courseRepository.save(course);
+        Course courseB = Course.builder()
+                .name("데이타구조")
+                .code("ECE20010")
+                .professor("김호준")
+                .courseYear(2023)
+                .semester(1)
+                .build();
+        courseRepository.save(courseB);
+        Course courseC = Course.builder()
+                .name("자바프로그래밍언어")
+                .code("ECE20017")
+                .professor("남재창")
+                .courseYear(2023)
+                .semester(1)
+                .build();
+        courseRepository.save(courseC);
         ReportForm form = ReportForm.builder()
                 .title("title")
                 .content("content")
@@ -37,7 +64,6 @@ public class ReportServiceTest {
                 .participants(List.of("22000328"))
                 .courses(List.of(1L,2L,3L))
                 .build();
-
         User user = User.builder()
                 .id("123")
                 .sid("22000328")
