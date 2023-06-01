7a3b15f1ebb35bddc917385f2036620ce6a23408
juyoung88 <76582732+juyoung88@users.noreply.github.com>
2023-06-02 04:46:44 +0900

    Fix: Fixes API url (#34)

--- src/main/java/edu/handong/csee/histudy/controller/ReportController.java
+++ src/main/java/edu/handong/csee/histudy/controller/ReportController.java
@@ -19,7 +19,7 @@ public class ReportController {
                                                  @RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken) {
         return reportService.createReport(form,accessToken);
     }
-    @GetMapping("/api/report/detailReport/{id}")
+    @GetMapping("/api/reports/detailReport/{id}")
     public ReportDto.Detail getDetailReport(@PathVariable long id,
                                             @RequestAttribute Claims claims) {
         return reportService.getReportDetail(id,claims.getSubject());
