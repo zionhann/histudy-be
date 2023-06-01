c008890ba90baea3c42532667258aee9fd9066ea
juyoung88 <76582732+juyoung88@users.noreply.github.com>
2023-06-02 03:59:24 +0900

    Add: Added new controller for admin use (#34)

---
+++ src/main/java/edu/handong/csee/histudy/controller/AdminController.java
@@ -0,0 +1,34 @@
+package edu.handong.csee.histudy.controller;
+
+import edu.handong.csee.histudy.dto.TeamDto;
+import edu.handong.csee.histudy.dto.TeamIdDto;
+import edu.handong.csee.histudy.dto.TeamReportDto;
+import edu.handong.csee.histudy.service.TeamService;
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/admin")
+public class AdminController {
+    private final TeamService teamService;
+
+    @GetMapping("/manageGroup")
+    public ResponseEntity<List<TeamDto>> getTeams(@RequestAttribute Claims claims) {
+        return ResponseEntity.ok(teamService.getTeams(claims.getSubject()));
+    }
+    @DeleteMapping("/group")
+    public ResponseEntity<Integer> deleteTeam(@RequestBody TeamIdDto dto, @RequestAttribute Claims claims) {
+        return ResponseEntity.ok(teamService.deleteTeam(dto, claims.getSubject()));
+    }
+    @GetMapping("/groupReport")
+    public ResponseEntity<TeamReportDto> getTeamReports(@RequestBody TeamIdDto dto,
+                                                              @RequestAttribute Claims claims) {
+        return ResponseEntity.ok(teamService.getTeamReports(dto, claims.getSubject()));
+    }
+}
