8c4b6f455fbc824ed1eb40359f010e1817bc63ac
juyoung88 <76582732+juyoung88@users.noreply.github.com>
2023-06-02 04:38:36 +0900

    Feat: Created new API and cleaned up code (#34)

--- src/main/java/edu/handong/csee/histudy/controller/ReportController.java
+++ src/main/java/edu/handong/csee/histudy/controller/ReportController.java
@@ -1,41 +1,27 @@
 package edu.handong.csee.histudy.controller;
 
 import edu.handong.csee.histudy.controller.form.ReportForm;
-import edu.handong.csee.histudy.domain.Report;
-import edu.handong.csee.histudy.domain.User;
 import edu.handong.csee.histudy.dto.ReportDto;
-import edu.handong.csee.histudy.repository.ReportRepository;
-import edu.handong.csee.histudy.repository.UserRepository;
 import edu.handong.csee.histudy.service.ReportService;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 public class ReportController {
 
-//    private final ReportRepository reportRepository;
     private final ReportService reportService;
-//    private final UserRepository userRepository;
 
-//    @PostMapping("/api/v1/report")
-//    public ReportDto.Response createReport(@RequestBody ReportForm form,
-//                                           @RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken) {
-//        User user = userRepository.findUserByAccessToken(accessToken).orElseThrow();
-//        List<Optional<User>> participants = form.getParticipants().stream()
-//                .map(userRepository::findUserBySid)
-//                .toList();
-//        Report report = form.toEntity(user.getTeam(), participants);
-//
-//        return new ReportDto.Response(reportRepository.save(report));
-//    }
     @PostMapping("/api/v1/report")
     public ReportDto.Response createReportCourse(@RequestBody ReportForm form,
                                                  @RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken) {
         return reportService.createReport(form,accessToken);
+    }
+    @GetMapping("/api/report/detailReport/{id}")
+    public ReportDto.Detail getDetailReport(@PathVariable long id,
+                                            @RequestAttribute Claims claims) {
+        return reportService.getReportDetail(id,claims.getSubject());
     }
 }
