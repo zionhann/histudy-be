fc83eac06736bda988363c987fac4cb36fe6ea1a
juyoung88 <76582732+juyoung88@users.noreply.github.com>
2023-06-02 04:33:47 +0900

    Add: Added imageDto (#34)

---
+++ src/main/java/edu/handong/csee/histudy/dto/ImageDto.java
@@ -0,0 +1,15 @@
+package edu.handong.csee.histudy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@NoArgsConstructor
+@Setter
+@AllArgsConstructor
+public class ImageDto {
+    private long id;
+    private String url;
+}
