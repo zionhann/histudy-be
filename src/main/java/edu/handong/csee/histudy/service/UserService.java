1de64b9542931060c20869f5a496ba7bef7d6a1e
juyoung88 <76582732+juyoung88@users.noreply.github.com>
2023-06-02 00:12:25 +0900

    Feat: created user list function in service (#34)

--- src/main/java/edu/handong/csee/histudy/service/UserService.java
+++ src/main/java/edu/handong/csee/histudy/service/UserService.java
@@ -3,16 +3,16 @@ package edu.handong.csee.histudy.service;
 import edu.handong.csee.histudy.controller.form.ApplyForm;
 import edu.handong.csee.histudy.controller.form.BuddyForm;
 import edu.handong.csee.histudy.controller.form.UserInfo;
-import edu.handong.csee.histudy.domain.Course;
-import edu.handong.csee.histudy.domain.Friendship;
-import edu.handong.csee.histudy.domain.Role;
-import edu.handong.csee.histudy.domain.User;
+import edu.handong.csee.histudy.domain.*;
+import edu.handong.csee.histudy.dto.CourseIdNameDto;
+import edu.handong.csee.histudy.dto.UserDto;
 import edu.handong.csee.histudy.repository.CourseRepository;
 import edu.handong.csee.histudy.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -101,5 +101,40 @@ public class UserService {
 
     public Optional<User> isPresent(String sub) {
         return userRepository.findById(sub);
+    }
+
+    public List<UserDto.Info> getUsers(String email) {
+        List<User> users = userRepository.findAll();
+        return users.stream()
+                .map(u -> {
+                    List<User> friends = new ArrayList<>();
+                    friends.addAll(u.getSentRequests()
+                            .stream()
+                            .filter(Friendship::isAccepted)
+                            .map(Friendship::getReceived)
+                            .toList());
+                    friends.addAll(u.getReceivedRequests()
+                            .stream()
+                            .filter(Friendship::isAccepted)
+                            .map(Friendship::getSent)
+                            .toList());
+                    List<UserDto.Basic> buddies = friends.stream().map(f -> UserDto.Basic.builder()
+                            .id(f.getId())
+                            .sid(f.getSid())
+                            .name(f.getName())
+                            .build()).toList();
+                    List<CourseIdNameDto> courses = u.getChoices()
+                            .stream()
+                            .map(Choice::getCourse)
+                            .map(Course::toIdNameDto)
+                            .toList();
+                    return UserDto.Info.builder()
+                            .id(u.getId())
+                            .sid(u.getSid())
+                            .name(u.getName())
+                            .friends(buddies)
+                            .courses(courses)
+                            .build();
+                }).toList();
     }
 }
