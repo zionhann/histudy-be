48aef78522c07dcd116827fed31d1928b5287bcd
zionhann <hanzion1108@gmail.com>
2024-03-01 21:59:36 +0900

    feat: 그룹 멤버 엔티티에 시간 정보 추가 (#149)

--- src/main/java/edu/handong/csee/histudy/domain/GroupMember.java
+++ src/main/java/edu/handong/csee/histudy/domain/GroupMember.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class GroupMember {
+public class GroupMember extends BaseTime {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
