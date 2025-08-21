bd2ed75e70d789081243f10f66aee64db2a5eb0e
zionhann <its.zionhan@gmail.com>
2025-08-21 23:18:01 +0900

    fix: 예외처리 컨트롤러 추가

--- src/test/java/edu/handong/csee/histudy/controller/AuthControllerTest.java
+++ src/test/java/edu/handong/csee/histudy/controller/AuthControllerTest.java
@@ -23,7 +23,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(AuthController.class)
+@WebMvcTest({AuthController.class, ExceptionController.class})
 class AuthControllerTest {
 
   @Autowired private MockMvc mockMvc;
--- src/test/java/edu/handong/csee/histudy/controller/PublicControllerTest.java
+++ src/test/java/edu/handong/csee/histudy/controller/PublicControllerTest.java
@@ -18,7 +18,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(PublicController.class)
+@WebMvcTest({PublicController.class, ExceptionController.class})
 class PublicControllerTest {
 
   @Autowired private MockMvc mockMvc;
